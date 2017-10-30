38628d4b82ee14b16e222a2d1394b37ab044cd85
Richard Mercer <rich@richmercer.com>
2017-10-30 14:40:03 +0000

    Adding sample script

---
+++ DemoSampleScript.ps1
@@ -0,0 +1,21 @@
+﻿#Requires -Modules TelligentCommunityRest,TelligentCommunitySample
+## Start Configuration
+
+$CommunityUrl = 'http://sampletest.local/'
+$AdminUsername = 'admin'
+$ApiKey = 'abc123'
+
+## End Configuration
+
+
+$creds = New-CommunityCredential $CommunityUrl $AdminUsername $ApiKey
+
+
+$productGroup = New-CommunitySampleGroup -ParentGroupId 1 -GroupType Joinless -Name "Products" -Wiki 0 -Gallery 0 -Forum 0 -Blog 0 -Credential $creds
+$productOneGroup = New-CommunitySampleGroup -ParentGroupId $productGroup.Id -GroupType PublicOpen -Name "Product One" -Wiki 1 -Gallery 1 -Credential $creds
+$productTwoGroup = New-CommunitySampleGroup -ParentGroupId $productGroup.Id -GroupType PublicOpen -Name "Product Two" -Wiki 1 -Gallery 1 -Credential $creds
+$productThreeGroup = New-CommunitySampleGroup -ParentGroupId $productGroup.Id -GroupType PublicOpen -Name "Product Three" -Wiki 1 -Gallery 1 -Credential $creds
+
+Initialize-CommunitySampleGroup -GroupId $productOneGroup.Id -Credential $creds
+Initialize-CommunitySampleGroup -GroupId $productTwoGroup.Id -Credential $creds
+Initialize-CommunitySampleGroup -GroupId $productThreeGroup.Id -Credential $creds
